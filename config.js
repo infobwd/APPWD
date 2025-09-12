2b0553fad2bc191aca3227a13ba9fc404096e6be
infobwd <168699470+infobwd@users.noreply.github.com>
2025-09-12 21:45:07 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,29 +1,8 @@
-// // ====== CONFIG ======
-// export const SUPABASE_URL="https://jxebbwfofurrpchxzlqd.supabase.co";
-// export const SUPABASE_ANON_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp4ZWJid2ZvZnVycnBjaHh6bHFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc1MDk4MzksImV4cCI6MjA3MzA4NTgzOX0.9tNHoDIUuMTmbI_ktmEjroMr7HE9t1F9CWh6HamleBk";
-// export const LIFF_ID = "2006490627-nERN5a26";
-// export const PUBLIC_BASE = "/APPWD/";
-// export const PUBLIC_URL  = "https://infobwd.github.io/APPWD/"; // ต้องมี / ท้าย
-// // Branding
-// export const BRAND_TITLE = "APPWD";
-// export const BRAND_LOGO_URL = ""; // ใส่ลิงก์โลโก้ (ถ้าเว้นว่างจะขึ้น WD)
-// // School geofence
-// export const SCHOOL_LAT = 14.301442009490573;
-// export const SCHOOL_LNG = 101.30579513744982;
-// export const SCHOOL_RADIUS_METERS = 200;
-
-// // Initial UI prefs
-// export const DEFAULT_FONT_SCALE = 1;   // 0.9 | 1 | 1.1
-// export const DEFAULT_ICON_SCALE = 1;   // 0.9 | 1 | 1.1
-// export const DEFAULT_THEME = "light";  // 'light' | 'dark' | 'system'
-
-// try{ localStorage.setItem("APPWD_PUBLIC_URL", PUBLIC_URL); }catch(e){}
- 
 export const SUPABASE_URL="https://jxebbwfofurrpchxzlqd.supabase.co";
 export const SUPABASE_ANON_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp4ZWJid2ZvZnVycnBjaHh6bHFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc1MDk4MzksImV4cCI6MjA3MzA4NTgzOX0.9tNHoDIUuMTmbI_ktmEjroMr7HE9t1F9CWh6HamleBk";
 export const LIFF_ID="2006490627-nERN5a26";
 export const PUBLIC_URL="https://infobwd.github.io/APPWD/";
-export const BRAND_TITLE="APPWD"; export const BRAND_LOGO_URL="https://raw.githubusercontent.com/infobwd/APPWD/refs/heads/main/icons/icon-192.png";
+export const BRAND_TITLE="APPWD"; export const BRAND_LOGO_URL="";
 export const SCHOOL_LAT=14.221816, SCHOOL_LNG=99.472859, SCHOOL_RADIUS_METERS=200000;
 export const DEFAULT_FONT_SCALE=1, DEFAULT_ICON_SCALE=1, DEFAULT_THEME="light";
 try{ localStorage.setItem("APPWD_PUBLIC_URL", PUBLIC_URL); }catch(e){}
