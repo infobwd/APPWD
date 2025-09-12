08f85c2820074a0eea43dcceea81f260f9f111bf
infobwd <168699470+infobwd@users.noreply.github.com>
2025-09-12 19:27:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,12 +18,12 @@
 // export const DEFAULT_THEME = "light";  // 'light' | 'dark' | 'system'
 
 // try{ localStorage.setItem("APPWD_PUBLIC_URL", PUBLIC_URL); }catch(e){}
-
+ 
 export const SUPABASE_URL="https://jxebbwfofurrpchxzlqd.supabase.co";
 export const SUPABASE_ANON_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp4ZWJid2ZvZnVycnBjaHh6bHFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc1MDk4MzksImV4cCI6MjA3MzA4NTgzOX0.9tNHoDIUuMTmbI_ktmEjroMr7HE9t1F9CWh6HamleBk";
 export const LIFF_ID="2006490627-nERN5a26";
 export const PUBLIC_URL="https://infobwd.github.io/APPWD/";
 export const BRAND_TITLE="APPWD"; export const BRAND_LOGO_URL="";
-export const SCHOOL_LAT=14.043918159922056, SCHOOL_LNG=99.58237599033546, SCHOOL_RADIUS_METERS=200;
+export const SCHOOL_LAT=14.221816, SCHOOL_LNG=99.472859, SCHOOL_RADIUS_METERS=200000;
 export const DEFAULT_FONT_SCALE=1, DEFAULT_ICON_SCALE=1, DEFAULT_THEME="light";
 try{ localStorage.setItem("APPWD_PUBLIC_URL", PUBLIC_URL); }catch(e){}
