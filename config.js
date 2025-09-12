12a277f80461a81cc0dc285de81111269990ed26
infobwd <168699470+infobwd@users.noreply.github.com>
2025-09-12 23:52:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ export const SUPABASE_ANON_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJ
 export const LIFF_ID="2006490627-nERN5a26";
 export const PUBLIC_URL="https://infobwd.github.io/APPWD/";
 export const BRAND_TITLE="APPWD"; export const BRAND_LOGO_URL="";
-export const SCHOOL_LAT=14.221816, SCHOOL_LNG=99.472859, SCHOOL_RADIUS_METERS=200000;
+export const SCHOOL_LAT=14.221816, SCHOOL_LNG=99.472859, SCHOOL_RADIUS_METERS=200;
 export const DEFAULT_FONT_SCALE=1, DEFAULT_ICON_SCALE=1, DEFAULT_THEME="light";
 try{ localStorage.setItem("APPWD_PUBLIC_URL", PUBLIC_URL); }catch(e){}
