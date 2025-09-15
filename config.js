a4070e5658acf5d45f3260489447bc0b3d267ca2
infobwd <168699470+infobwd@users.noreply.github.com>
2025-09-15 22:37:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-export const SUPABASE_URL="https://YOUR-PROJECT.supabase.co";
-export const SUPABASE_ANON_KEY="YOUR-ANON-KEY";
+export const SUPABASE_URL="https://jxebbwfofurrpchxzlqd.supabase.co";
+export const SUPABASE_ANON_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp4ZWJid2ZvZnVycnBjaHh6bHFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc1MDk4MzksImV4cCI6MjA3MzA4NTgzOX0.9tNHoDIUuMTmbI_ktmEjroMr7HE9t1F9CWh6HamleBk";
 export const LIFF_ID="2006490627-nERN5a26";
 export const PUBLIC_URL="https://infobwd.github.io/APPWD/";
 export const BRAND_TITLE="APPWD"; export const BRAND_LOGO_URL="";
