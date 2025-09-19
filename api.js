2000f6fa90f80f68b5a3e7c79bf4ba9f94ebf994
infobwd <168699470+infobwd@users.noreply.github.com>
2025-09-19 10:25:41 +0700

    Update api.js

--- api.js
+++ api.js
@@ -1,3 +1,4 @@
+/*api.js*/
 import { SUPABASE_URL,SUPABASE_ANON_KEY } from './config.js';
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 export const supabase=createClient(SUPABASE_URL,SUPABASE_ANON_KEY);
